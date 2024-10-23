eb416fa1fb6bd9de5e7a537ab147e52dacb1275f
wiut132777 <113832224+wiut13277@users.noreply.github.com>
2024-10-23 23:30:00 +0500

    add crud api

--- API_13277/API_13277/DAL/Employee.cs
+++ API_13277/API_13277/DAL/Employee.cs
@@ -2,5 +2,11 @@
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public decimal Salary { get; set; }
+        public string Position { get; set; }
     }
 }
--- API_13277/API_13277/Program.cs
+++ API_13277/API_13277/Program.cs
@@ -1,3 +1,6 @@
+using API_13277.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<EmployeeContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(o =>
+    o.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
 
